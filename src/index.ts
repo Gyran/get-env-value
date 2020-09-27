64b4496d899ec4c0dad2fd29e6ecfd569557570f
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2020-09-27 23:10:48 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+export const stringValue = (key: string, defaultValue: string = '') => {
+  const envValue = process.env[key];
+  if (envValue) {
+    return envValue;
+  }
+
+  return defaultValue;
+};
+
+export const integerValue = (key: string, defaultValue = 0): number => {
+  const envValue = stringValue(key, '');
+  if (typeof envValue === 'string') {
+    const intValue = Number.parseInt(envValue, 10);
+    if (Number.isInteger(intValue)) {
+      return intValue;
+    }
+  }
+
+  return defaultValue;
+};
+
+export const booleanValue = (key: string, defaultValue = false) => {
+  const envValue = stringValue(key);
+
+  if (envValue === 'true') {
+    return true;
+  }
+
+  if (envValue === 'false') {
+    return false;
+  }
+
+  return defaultValue;
+};
+
+function parsedJSONValue<T>(key: string): T | undefined;
+function parsedJSONValue<T>(key: string, defaultValue: T): T;
+function parsedJSONValue<T>(key: string, defaultValue?: T): T | undefined {
+  const envValue = stringValue(key);
+  if (envValue) {
+    try {
+      return JSON.parse(envValue);
+    } catch (error) {
+      return defaultValue;
+    }
+  }
+
+  return defaultValue;
+}
+export { parsedJSONValue };
